ffb540308b224ec1a2e282ffc802131370ca1f3e
Zohaib <zohaibsoomro100@gmail.com>
2023-05-30 00:03:42 +0500

    Changes Made

---
+++ src/components/ProjectsData.js
@@ -0,0 +1,38 @@
+
+const projectData = [
+  {
+    date: "19th March, 2022",
+    description:
+      "Joined, Sir Muhammad Ali Mughal's Certified Web 3.0 and Metaverse Course by Jawan Pakistan. Alhamdulillah. &#x2764;",
+    projectPicture:
+      "./Images/logo.png",
+  },
+  {
+    date: "15th March, 2022",
+    description: "Hello, this is the dummy description",
+    projectPicture:
+      "./Images/333720800_943284120013635_9211563476470379960_n.jpg"
+  },
+  {
+    date: "29th May, 2022",
+    description: "Hello, this is the dummy description",
+    projectPicture:
+      "./Images/333720800_943284120013635_9211563476470379960_n.jpg",
+  },
+  {
+    date: "7th December, 2022",
+    description: "Hello, this is the dummy description",
+    projectPicture:
+      "./Images/333720800_943284120013635_9211563476470379960_n.jpg",
+  },
+  {
+    date: "18th April, 2022",
+    description: "Hello, this is the dummy description",
+    projectPicture:
+      "./Images/333720800_943284120013635_9211563476470379960_n.jpg",
+  },
+];
+
+
+
+export default projectData;
