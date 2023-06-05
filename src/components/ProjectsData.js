cad40e4975e14877bd8aa221529eec060bf781f1
Zohaib <zohaibsoomro100@gmail.com>
2023-06-05 22:39:26 +0500

    Responsiveness Added

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -1,16 +1,18 @@
+import postPicture from"../components/Images/zohaib's picture.png"
+
 const projectData = [
   {
     date: "19th March, 2022",
     description:
       "Joined, Sir Muhammad Ali Mughal's Certified Web 3.0 and Metaverse Course by Jawan Pakistan. Alhamdulillah. &#x2764;",
     projectPicture: "./src/components/Images/logo.png",
-    accountPicture: "./src/components/Images/zohaib's picture.png", 
+    accountPicture: postPicture
   },
   {
     date: "15th March, 2022",
     description: "Hello, this is the dummy description",
     projectPicture: "./src/components/Images/logo.png",
-    accountPicture: "./src/components/Images/zohaib's picture.png",
+    accountPicture: "./src/components/Images/zohaib's picture.png"
   },
   {
     date: "29th May, 2022",
