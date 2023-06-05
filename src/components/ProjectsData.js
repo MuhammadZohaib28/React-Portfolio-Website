77a73cdaf8462bbb2d3b79d1c1cb3d43241570ec
Zohaib <zohaibsoomro100@gmail.com>
2023-06-05 22:43:51 +0500

    project section bugs removed

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -1,36 +1,37 @@
-import postPicture from"../components/Images/zohaib's picture.png"
+import postPicture from "../components/Images/zohaib's picture.png";
+import projectPicture from "../components/Images/logo.png";
 
 const projectData = [
   {
     date: "19th March, 2022",
     description:
       "Joined, Sir Muhammad Ali Mughal's Certified Web 3.0 and Metaverse Course by Jawan Pakistan. Alhamdulillah. &#x2764;",
-    projectPicture: "./src/components/Images/logo.png",
-    accountPicture: postPicture
+    projectPicture: projectPicture,
+    accountPicture: postPicture,
   },
   {
     date: "15th March, 2022",
     description: "Hello, this is the dummy description",
-    projectPicture: "./src/components/Images/logo.png",
-    accountPicture: "./src/components/Images/zohaib's picture.png"
+    projectPicture: projectPicture,
+    accountPicture: postPicture,
   },
   {
     date: "29th May, 2022",
     description: "Hello, this is the dummy description",
-    projectPicture: "./src/components/Images/logo.png",
-    accountPicture: "./src/components/Images/zohaib's picture.png",
+    projectPicture: projectPicture,
+    accountPicture: postPicture,
   },
   {
     date: "7th December, 2022",
     description: "Hello, this is the dummy description",
-    projectPicture: "./src/components/Images/logo.png",
-    accountPicture: "./src/components/Images/zohaib's picture.png",
+    projectPicture: projectPicture,
+    accountPicture: postPicture,
   },
   {
     date: "18th April, 2022",
     description: "Hello, this is the dummy description",
-    projectPicture: "./src/components/Images/logo.png",
-    accountPicture: "./src/components/Images/zohaib's picture.png",
+    projectPicture: projectPicture,
+    accountPicture: postPicture,
   },
 ];
 
