27b532dc45705bf68f64c10ae5fb589717ee1fa4
Zohaib <zohaibsoomro100@gmail.com>
2023-06-26 15:18:52 +0500

    Resume and Hire me added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./header.css";
 import logo from "./Images/logo.png";
+import cvFile from "./path/to/Muhammad Zohaib Resume.pdf";
 
 const Header = () => {
+  const handleHireMeClick = () => {
+    window.open("mailto:zohaibsoomro0101@gmail.com", "_blank");
+  };
+
+  const handleDownloadCVClick = () => {
+    window.open(cvFile, "_blank");
+  };
+
   return (
     <div className="headder">
       <header class="header">
@@ -10,9 +19,8 @@ const Header = () => {
 
         <nav>
           <ul>
-            <li>Hire Me</li>
-            <li>Download CV</li>
-            <li>About Me</li>
+            <li onClick={handleHireMeClick}>Hire Me</li>
+            <li onClick={handleDownloadCVClick}>Download Resume</li>
           </ul>
         </nav>
       </header>
