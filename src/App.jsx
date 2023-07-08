e21ef50ee6326f22e515aebad149efc864a5330d
Zohaib <zohaibsoomro100@gmail.com>
2023-07-09 00:17:11 +0500

    Dark Mode Feature Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState }  from "react";
 import Header from "./components/Header";
 import Marquee from "./components/Marquee";
 import Hero from "./components/Hero";
@@ -6,8 +6,16 @@ import Projects from "./components/Projects";
 import "./App.css";
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.body.classList.toggle('dark-mode');
+  };
+  
   return (
     <div className="app">
+      <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
       <Header />
       <Marquee />
       <Hero />
