a9b11bc502e6d8d47a7994d047f1da944b4ac28a
Zohaib <zohaibsoomro100@gmail.com>
2023-07-19 01:56:45 +0500

    Project Added

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -5,53 +5,67 @@ import projectPicture3 from "../components/Images/shopingcart.jpg";
 import projectPicture4 from "../components/Images/quizapp.jpg";
 import projectPicture5 from "../components/Images/portfolio.jpg";
 import projectPicture6 from "../components/Images/shoewebsite.jpg";
-
+import projectPicture7 from "../components/Images/funnyexcuse.jpg";
 
 const projectData = [
-
   {
     date: "19th March, 2022",
-    description: "This project is one of my first project, Crypto Price App. Here you can see the updated prices of digital currencies. ",
+    description:
+      "This project is one of my first project, Crypto Price App. Here you can see the updated prices of digital currencies. ",
     projectPicture: projectPicture1,
     accountPicture: postPicture,
-    projectLink: "https://github.com/MuhammadZohaib28/CryptoPriceApp"
+    projectLink: "https://github.com/MuhammadZohaib28/CryptoPriceApp",
   },
   {
     date: "15th March, 2022",
-    description: "This is the ReactJs Social Media Website. Here I have use ReactJs and Material UI. From design to code totally done by mee.",
+    description:
+      "This is the ReactJs Social Media Website. Here I have use ReactJs and Material UI. From design to code totally done by mee.",
     projectPicture: projectPicture2,
     accountPicture: postPicture,
-    projectLink: "https://github.com/MuhammadZohaib28/socialmediawebsitefrontend"
+    projectLink:
+      "https://github.com/MuhammadZohaib28/socialmediawebsitefrontend",
   },
   {
     date: "10th June, 2023",
     description: "This project is conversion of a figma design to a ReactJS. ",
     projectPicture: projectPicture6,
     accountPicture: postPicture,
-    projectLink: "https://github.com/MuhammadZohaib28/FigmaToReactJS"
+    projectLink: "https://github.com/MuhammadZohaib28/FigmaToReactJS",
+  },
+  {
+    date: "19th June, 2023",
+    description:
+      "This is the Funny Excuse Generator using ReactJS and free API. ",
+    projectPicture: projectPicture7,
+    accountPicture: postPicture,
+    projectLink: "https://funnyexcusegenerator.vercel.app/",
   },
   {
     date: "29th May, 2022",
-    description: "This is the React Shopping Cart Ecommerce Website for Beginners.",
+    description:
+      "This is the React Shopping Cart Ecommerce Website for Beginners.",
     projectPicture: projectPicture3,
     accountPicture: postPicture,
-    projectLink: "https://github.com/MuhammadZohaib28/ShoppingCartECommerceWebsite"
+    projectLink:
+      "https://github.com/MuhammadZohaib28/ShoppingCartECommerceWebsite",
   },
   {
     date: "7th December, 2022",
-    description: "This is the quiz app using ReactJs and used vanilla CSS, Here you can learn alot about Hooks Context API and how React Works",
+    description:
+      "This is the quiz app using ReactJs and used vanilla CSS, Here you can learn alot about Hooks Context API and how React Works",
     projectPicture: projectPicture4,
     accountPicture: postPicture,
-    projectLink: "https://github.com/MuhammadZohaib28/QuizAppBeginners"
+    projectLink: "https://github.com/MuhammadZohaib28/QuizAppBeginners",
   },
   {
     date: "18th April, 2022",
-    description: "This is the portfolio website which was created by me using HTML, CSS and JS. This is the beginner level Portfolio Website.",
+    description:
+      "This is the portfolio website which was created by me using HTML, CSS and JS. This is the beginner level Portfolio Website.",
     projectPicture: projectPicture5,
     accountPicture: postPicture,
-    projectLink: "https://github.com/MuhammadZohaib28/PortfolioWebsiteUsingHTML-CSS-JS"
+    projectLink:
+      "https://github.com/MuhammadZohaib28/PortfolioWebsiteUsingHTML-CSS-JS",
   },
-  
 ];
 
 export default projectData;
